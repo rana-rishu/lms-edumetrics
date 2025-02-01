303f2c6f924e24081c1ce7a56858dfeeeb26817b
rishu rana <mr.rishurana@gmail.com>
2025-02-01 06:21:31 +0530

    Update firebase.jsx

--- edumetrics (1)/edumetrics/src/firebase.jsx
+++ edumetrics (1)/edumetrics/src/firebase.jsx
@@ -5,14 +5,6 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAPXboBgG27FOexXC1x2fmRrNNr1GM3II4",
-  authDomain: "edumetrics-699ab.firebaseapp.com",
-  projectId: "edumetrics-699ab",
-  storageBucket: "edumetrics-699ab.firebasestorage.app",
-  messagingSenderId: "1044640771649",
-  appId: "1:1044640771649:web:0644163c81fd6c6c1ba0c0"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
